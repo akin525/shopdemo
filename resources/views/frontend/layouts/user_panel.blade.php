0108abde41999f73787042366d1fc3fff41346b3
PIPER <akinlabi@gmail.com>
2024-03-03 06:07:26 +0100

    zip

--- resources/views/frontend/layouts/user_panel.blade.php
+++ resources/views/frontend/layouts/user_panel.blade.php
@@ -4,9 +4,9 @@
     <div class="container">
         <div class="d-flex align-items-start">
 			@include('frontend.inc.user_side_nav')
-			<div class="aiz-user-panel">
-				@yield('panel_content')
-            </div>
+{{--			<div class="aiz-user-panel">--}}
+{{--				@yield('panel_content')--}}
+{{--            </div>--}}
         </div>
     </div>
 </section>
