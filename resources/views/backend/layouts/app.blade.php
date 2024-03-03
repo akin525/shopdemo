95bee3caff6f9e77da930eecde7e4a57965cb368
PIPER <akinlabi@gmail.com>
2024-03-03 06:02:04 +0100

    zip

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -84,53 +84,53 @@
 
     @yield('script')
 
-    <script type="text/javascript">
-	    @foreach (session('flash_notification', collect())->toArray() as $message)
-	        AIZ.plugins.notify('{{ $message['level'] }}', '{{ $message['message'] }}');
-	    @endforeach
+{{--    <script type="text/javascript">--}}
+{{--	    @foreach (session('flash_notification', collect())->toArray() as $message)--}}
+{{--	        AIZ.plugins.notify('{{ $message['level'] }}', '{{ $message['message'] }}');--}}
+{{--	    @endforeach--}}
 
 
-        if ($('#lang-change').length > 0) {
-            $('#lang-change .dropdown-menu a').each(function() {
-                $(this).on('click', function(e){
-                    e.preventDefault();
-                    var $this = $(this);
-                    var locale = $this.data('flag');
-                    $.post('{{ route('language.change') }}',{_token:'{{ csrf_token() }}', locale:locale}, function(data){
-                        location.reload();
-                    });
+{{--        if ($('#lang-change').length > 0) {--}}
+{{--            $('#lang-change .dropdown-menu a').each(function() {--}}
+{{--                $(this).on('click', function(e){--}}
+{{--                    e.preventDefault();--}}
+{{--                    var $this = $(this);--}}
+{{--                    var locale = $this.data('flag');--}}
+{{--                    $.post('{{ route('language.change') }}',{_token:'{{ csrf_token() }}', locale:locale}, function(data){--}}
+{{--                        location.reload();--}}
+{{--                    });--}}
 
-                });
-            });
-        }
-        function menuSearch(){
-			var filter, item;
-			filter = $("#menu-search").val().toUpperCase();
-			items = $("#main-menu").find("a");
-			items = items.filter(function(i,item){
-				if($(item).find(".aiz-side-nav-text")[0].innerText.toUpperCase().indexOf(filter) > -1 && $(item).attr('href') !== '#'){
-					return item;
-				}
-			});
+{{--                });--}}
+{{--            });--}}
+{{--        }--}}
+{{--        function menuSearch(){--}}
+{{--			var filter, item;--}}
+{{--			filter = $("#menu-search").val().toUpperCase();--}}
+{{--			items = $("#main-menu").find("a");--}}
+{{--			items = items.filter(function(i,item){--}}
+{{--				if($(item).find(".aiz-side-nav-text")[0].innerText.toUpperCase().indexOf(filter) > -1 && $(item).attr('href') !== '#'){--}}
+{{--					return item;--}}
+{{--				}--}}
+{{--			});--}}
 
-			if(filter !== ''){
-				$("#main-menu").addClass('d-none');
-				$("#search-menu").html('')
-				if(items.length > 0){
-					for (i = 0; i < items.length; i++) {
-						const text = $(items[i]).find(".aiz-side-nav-text")[0].innerText;
-						const link = $(items[i]).attr('href');
-						 $("#search-menu").append(`<li class="aiz-side-nav-item"><a href="${link}" class="aiz-side-nav-link"><i class="las la-ellipsis-h aiz-side-nav-icon"></i><span>${text}</span></a></li`);
-					}
-				}else{
-					$("#search-menu").html(`<li class="aiz-side-nav-item"><span	class="text-center text-muted d-block">{{ translate('Nothing Found') }}</span></li>`);
-				}
-			}else{
-				$("#main-menu").removeClass('d-none');
-				$("#search-menu").html('')
-			}
-        }
-    </script>
+{{--			if(filter !== ''){--}}
+{{--				$("#main-menu").addClass('d-none');--}}
+{{--				$("#search-menu").html('')--}}
+{{--				if(items.length > 0){--}}
+{{--					for (i = 0; i < items.length; i++) {--}}
+{{--						const text = $(items[i]).find(".aiz-side-nav-text")[0].innerText;--}}
+{{--						const link = $(items[i]).attr('href');--}}
+{{--						 $("#search-menu").append(`<li class="aiz-side-nav-item"><a href="${link}" class="aiz-side-nav-link"><i class="las la-ellipsis-h aiz-side-nav-icon"></i><span>${text}</span></a></li`);--}}
+{{--					}--}}
+{{--				}else{--}}
+{{--					$("#search-menu").html(`<li class="aiz-side-nav-item"><span	class="text-center text-muted d-block">{{ translate('Nothing Found') }}</span></li>`);--}}
+{{--				}--}}
+{{--			}else{--}}
+{{--				$("#main-menu").removeClass('d-none');--}}
+{{--				$("#search-menu").html('')--}}
+{{--			}--}}
+{{--        }--}}
+{{--    </script>--}}
 
 </body>
 </html>
