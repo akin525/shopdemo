b8872bdf7ea2c8ec592502d5ac7e9a556ef7d11e
PIPER <akinlabi@gmail.com>
2024-03-03 06:11:10 +0100

    zip

--- resources/views/backend/layouts/blank.blade.php
+++ resources/views/backend/layouts/blank.blade.php
@@ -35,10 +35,10 @@
 
     @yield('script')
 
-    <script type="text/javascript">
-    @foreach (session('flash_notification', collect())->toArray() as $message)
-        AIZ.plugins.notify('{{ $message['level'] }}', '{{ $message['message'] }}');
-    @endforeach
-    </script>
+{{--    <script type="text/javascript">--}}
+{{--    @foreach (session('flash_notification', collect())->toArray() as $message)--}}
+{{--        AIZ.plugins.notify('{{ $message['level'] }}', '{{ $message['message'] }}');--}}
+{{--    @endforeach--}}
+{{--    </script>--}}
 </body>
 </html>
